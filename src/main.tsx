e38a2b6c5c1e4b36e59b87368d8142a80c09fd3f
David Johnston <davidajohnston2000@gmail.com>
2025-04-29 00:37:10 +0100

    Add authentication

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider
+      domain={'dajohnston.eu.auth0.com'}
+      clientId={'IgckwgORISVWwBUI7BQSD2AtIlL2onsX'}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
   </StrictMode>,
-)
+);
