bf7ead9a7a73c4e648f3b48b68214f29f2d09a8e
David Johnston <davidajohnston2000@gmail.com>
2025-05-02 23:00:59 +0100

    Dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react';
-import { lazy, useState } from 'react';
+import { lazy, Suspense, useState } from 'react';
+import { Button, Container, Typography } from '@mui/material';
 import logo from './assets/logo.png';
 
 // eslint-disable-next-line import-x/no-unresolved
@@ -11,50 +12,48 @@ function App() {
   const [logoutFailed, setLogoutFailed] = useState(false);
   const [loginFailed, setLoginFailed] = useState(false);
 
-  if (loginFailed) {
-    return <p>Login failed</p>;
-  }
-  if (logoutFailed) {
-    return <p>Logout failed</p>;
-  }
   if (isLoading) {
     return <div>Loading...</div>;
   }
   return (
-    <>
-      <SpiceTracker />
-      {isAuthenticated && user ? (
-        <div>
-          <img src={logo} alt={'logo'} width={'32px'} height={'32px'} />
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-          <button
-            type="button"
-            onClick={() => {
-              logout({
-                logoutParams: { returnTo: window.location.origin },
-              }).catch(() => setLogoutFailed(true));
-              return undefined;
-            }}
-          >
-            Log Out
-          </button>
-        </div>
-      ) : (
-        <div>
-          <button
-            type="button"
-            onClick={() => {
-              loginWithRedirect().catch(() => setLoginFailed(true));
-              return undefined;
-            }}
-          >
-            Log In
-          </button>
-        </div>
-      )}
-    </>
+    <Container maxWidth={'md'}>
+      <Suspense fallback={<div>Loading...</div>}>
+        <SpiceTracker />
+        {loginFailed && <p>Login failed</p>}
+        {logoutFailed && <p>Logout failed</p>}
+        {isAuthenticated && user ? (
+          <div>
+            <img src={logo} alt={'logo'} width={'32px'} height={'32px'} />
+            <img src={user.picture} alt={user.name} />
+            <Typography variant={'h2'}>{user.name}</Typography>
+            <Typography>{user.email}</Typography>
+            <Button
+              variant="contained"
+              onClick={() => {
+                logout({
+                  logoutParams: { returnTo: window.location.origin },
+                }).catch(() => setLogoutFailed(true));
+                return undefined;
+              }}
+            >
+              Log Out
+            </Button>
+          </div>
+        ) : (
+          <div>
+            <Button
+              variant="contained"
+              onClick={() => {
+                loginWithRedirect().catch(() => setLoginFailed(true));
+                return undefined;
+              }}
+            >
+              Log In
+            </Button>
+          </div>
+        )}
+      </Suspense>
+    </Container>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
-import { CssBaseline } from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import App from './App';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -17,8 +23,10 @@ createRoot(document.getElementById('root')!).render(
         redirect_uri: window.location.origin,
       }}
     >
-      <CssBaseline />
-      <App />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </Auth0Provider>
   </StrictMode>,
 );
