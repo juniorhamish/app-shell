329517d09048a35d799db01f955dfef364b7aacc
David Johnston <davidajohnston2000@gmail.com>
2025-05-03 19:40:12 +0100

    Simplify App layout and styling.
    Removed redundant styles and streamlined the structure of the `<Container>`, `<Toolbar>`, and child components. Adjusted typography and button properties for consistency and cleaner code.

--- src/App.tsx
+++ src/App.tsx
@@ -10,24 +10,19 @@ import logo from './assets/logo-round.png';
 
 function App() {
   return (
-    <Container
-      maxWidth="lg"
-      disableGutters
-      component="main"
-      sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-    >
+    <Container maxWidth="lg" disableGutters component="main">
       <AppBar position="static">
-        <Toolbar disableGutters>
+        <Toolbar>
           <Box
             sx={{ maxWidth: 60, verticalAlign: 'middle' }}
             component="img"
             src={logo}
             alt="Logo"
           />
-          <Typography variant="h1" sx={{ fontSize: 18, fontWeight: 'bold' }}>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
             DAJohnston
           </Typography>
-          <Button sx={{ marginLeft: 'auto', marginRight: 5 }}>Sign in</Button>
+          <Button>Sign in</Button>
         </Toolbar>
       </AppBar>
     </Container>
